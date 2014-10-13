a6bddae92989ba99d7f3f20c9cb2edb346793a04
Jordan Curzon <curzonj@gmail.com>
2014-10-13 10:05:40 -0700

    remove ui stuff

--- src/server.js
+++ src/server.js
@@ -13,7 +13,6 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(logger('dev'));
-app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
